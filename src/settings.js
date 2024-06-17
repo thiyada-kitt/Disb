092e6737638fb411f1828f85bd81d47c96ef8886
thiyada-kitt <111549728+thiyada-kitt@users.noreply.github.com>
2024-06-18 04:14:31 +0700

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 export default {
-  CHANNEL_ID: '1162793937707208795',
+  CHANNEL_ID: '1251111769997574234',
   GUILD_ID: '1162793937707208795',
   PORT: process.env.PORT || 4000,
   origin: ['http://localhost:3000']
